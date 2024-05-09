5b77a1975c15b47b55e92bd0c4b818ae7d2deaa4
Gustavo Lara <gustavolara63719@gmail.com>
2024-05-09 10:33:55 -0300

    Adicionar arquivos de projeto.

---
+++ notes.ui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace notes.ui
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ notes.ui/Sobre.xaml.cs
@@ -0,0 +1,9 @@
+namespace notes.ui;
+
+public partial class Sobre : ContentPage
+{
+	public Sobre()
+	{
+		InitializeComponent();
+	}
+}
